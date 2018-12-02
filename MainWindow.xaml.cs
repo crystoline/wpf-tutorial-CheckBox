44f10ec40cc27a1507df082d23e48aa39649fda1
crystoline <crystoline@gmail.com and git remote add origin https://github.com/crystoline/wpf-tutorial-CheckBox.git>
2018-12-02 12:35:24 +0100

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCheckBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AllCheckChanged(object sender, RoutedEventArgs e)
+        {
+            bool state = (AllTopings.IsChecked == true);
+            Salami.IsChecked =
+             Mushroom.IsChecked =
+             Mozzarella.IsChecked = state;
+
+        }
+        private void SingleCheckChanged(object sender, RoutedEventArgs e)
+        {
+            AllTopings.IsChecked = null;
+            if (Salami.IsChecked == true &&
+             Mushroom.IsChecked == true &&
+             Mozzarella.IsChecked == true)
+            {
+                AllTopings.IsChecked = true;
+            }
+
+            if (Salami.IsChecked == false &&
+             Mushroom.IsChecked == false &&
+             Mozzarella.IsChecked == false)
+            {
+                AllTopings.IsChecked = false;
+            }
+
+
+        }
+
+
+    }
+
+   
+}
